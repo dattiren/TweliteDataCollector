a7927397feb817b93cd708576727da90c5eab722
dattiren <musicdattiren@gmail.com>
2018-07-19 23:53:35 +0900

    modify to collect sleeping data

--- twelite2525_2.py
+++ twelite2525_2.py
@@ -5,53 +5,62 @@ from time import sleep
 import time
 from datetime import datetime
 
-devices = ['/dev/tty.usbserial-MWVK5MZ', '/dev/tty.usbserial-MWVK7NL', '/dev/tty.usbserial-MWVK5L4', '/dev/tty.usbserial-MWVK5L2']
-ser1 = serial.Serial(devices[0], 115200)
+devices = ['/dev/ttyUSB0', '/dev/ttyUSB1']
+#ser1 = serial.Serial(devices[0], 115200)
 ser2 = serial.Serial(devices[1], 115200)
-ser3 = serial.Serial(devices[2], 115200)
-ser4 = serial.Serial(devices[3], 115200)
+#ser3 = serial.Serial(devices[2], 115200)
+#ser4 = serial.Serial(devices[3], 115200)
 num=0
-childId1 = "ed=810F1C1C";
-childId2 = "ed=810F19CF";
-childId3 = "ed=810F1D12";
-childId4 = "ed=810F19B1";
+childId1 = "810F19B1";
+childId2 = "810F19CF";
+childId3 = "810F1D12";
+#childId4 = "ed=810F19B1";
+start = time.time()
 
 def output(filename, ser):
         now = datetime.now()
-        value = ser.readline().rstrip()
-        arr = value.split(":")
-        if(len(arr) == 13):
-            (nil, nil2,  rc, lq, ct, ed, num, ba, a1, a2, x, y, z) = value.split(":")
-            # (nil, ts, nil2, lq, ct, ed, ba, mn, nil3, a1, a2i, p, x, y, z, nil4) = value.split(";")
-            if(ed == childId1):
-                with open(filename + childId1 + ".csv", 'a') as f:
-                    writer = csv.writer(f, lineterminator='\n')
-                    a = [now, ct, lq, ed, x, y, z]
-                    writer.writerow(a)
-                    print(a)
-            elif(ed == childId2):
-                with open(filename + childId2 + ".csv", 'a') as f:
-                    writer = csv.writer(f, lineterminator='\n')
-                    a = [now, ct, lq, ed, x, y, z]
-                    writer.writerow(a)
-                    print(a)
-            elif(ed == childId3):
-                with open(filename + childId3 + ".csv", 'a') as f:
-                    writer = csv.writer(f, lineterminator='\n')
-                    a = [now, ct, lq, ed, x, y, z]
-                    writer.writerow(a)
-                    print(a)
-            elif(ed == childId4):
-                with open(filename + childId4 + ".csv", 'a') as f:
-                    writer = csv.writer(f, lineterminator='\n')
-                    a = [now, ct, lq, ed, x, y, z]
-                    writer.writerow(a)
-                    print(a)
+        while True:
+            value = ser.readline().rstrip().decode('utf-8')
+            arr = value.split(":")
+            if(len(arr) == 13):
+                list = ['ed=', 'lq=', 'ba=','x=','y=', 'z=']
+                for i in range(len(list)):
+                    value=value.replace(list[i], '')
+                (nil, nil2,  rc, lq, ct, ed, num, ba, a1, a2, x, y, z) = value.split(":")
+                # (nil, ts, nil2, lq, ct, ed, ba, mn, nil3, a1, a2i, p, x, y, z, nil4) = value.split(";")
+                if(ed == childId1):
+                    with open(filename + "center.csv", 'a') as f:
+                        writer = csv.writer(f, lineterminator='\n')
+                        ts = round(time.time() - start, 2)
+                        a = [ts, lq, ba, x, y, z]
+                        writer.writerow(a)
+                        print(a)
+                elif(ed == childId2):
+                    with open(filename + "right.csv", 'a') as f:
+                        writer2 = csv.writer(f, lineterminator='\n')
+                        ts2 = round(time.time() - start, 2)
+                        a2 = [ts2, lq, ba, x, y, z]
+                        writer2.writerow(a2)
+                        print(a2)
+                elif(ed == childId3):
+                    with open(filename + "left.csv", 'a') as f:
+                        writer3 = csv.writer(f, lineterminator='\n')
+                        ts3=round(time.time() - start, 2)
+                        a3 = [ts3, lq, ba, x, y, z]
+                        writer3.writerow(a3)
+                        print(a3)
+#            elif(ed == childId4):
+#                with open(filename + childId4 + ".csv", 'a') as f:
+#                    writer = csv.writer(f, lineterminator='\n')
+#                    a = [now, ct, lq, ed, x, y, z]
+#                    writer.writerow(a)
+#                    print(a)
 
-while True:
-    output("/Users/ren/Desktop/TweliteTest/twelite_back_R/",  ser1)
-    output("/Users/ren/Desktop/TweliteTest/twelite_front_L/", ser2)
-    output("/Users/ren/Desktop/TweliteTest/twelite_back_L/", ser3)
-    output("/Users/ren/Desktop/TweliteTest/twelite_front_R/", ser4)
+
+if __name__ == '__main__':
+    #output("/home/pi/Desktop/", ser1)
+    output("/home/pi/Desktop/", ser2)
+#    output("/home/pi/Desktop/", ser3)
+#    output("/Users/ren/Desktop/TweliteTest/twelite_front_R/", ser4)
     # /Users/ren/Desktop/TweliteTest/%s.csv
 
