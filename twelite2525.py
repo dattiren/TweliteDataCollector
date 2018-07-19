31379cb79bb523944068eb41c805e0fdc4c232b8
dattiren <ren@cps.im.dendai.ac.jp>
2018-07-19 21:18:17 +0900

    "remove value tag"

--- twelite2525.py
+++ twelite2525.py
@@ -5,17 +5,24 @@ from time import sleep
 import time
 from datetime import datetime
 
-# devices = '/dev/tty.usbserial-MW8UKP5'
-devices = 'your device port'
+devices = '/dev/tty.usbserial-MWVK5L2'
 ser = serial.Serial(devices, 115200)
+num=0
 while True:
     now = datetime.now()
-    with open("twelite2525.csv" ,'a') as f:   #ファイル名の設定
+    with open('/Users/ren/Desktop/demo.csv' ,'a') as f:
         writer = csv.writer(f, lineterminator='\n')
-        value = ser.readline().rstrip()
+        value = ser.readline().rstrip().decode('utf-8')
         arr = value.split(":")
+
         if(len(arr) == 13):
+            list = ['ed=', 'lq=', 'x=', 'y=', 'z=']
+            for i in range(len(list)):
+                value = value.replace(list[i], '')
             (nil, nil2,  rc, lq, ct, ed, num, ba, a1, a2, x, y, z) = value.split(":")
-            if(ed == "ed=810F0646"):
-                a = [now, ed, x, y, z]
-                writer.writerow(a)   #csvへの書き込み
+            writer.writerow(lq)
+           # (nil, ts, nil2, lq, ct, ed, ba, mn, nil3, a1, a2i, p, x, y, z, nil4) = value.split(";")
+           #810F1C1C,810F19CF ,810F1D12 
+            # a = [now, ct, lq, ed, ba, x, y, z]
+            #a = [lq]
+            print(lq)
